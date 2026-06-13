0c120c356fc988c9a390388c331c8d2a8db5486e
Jacob Habtemariam <jacob@mac.myfiosgateway.com>
2026-06-13 16:28:22 -0400

    test drizzle orm

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,39 @@
 // import Image from "next/image";
+import { Button } from "@/components/ui/button";
+import { db } from "@/db";
+import { pings } from "@/db/schema";
+import { desc } from "drizzle-orm";
+import { revalidatePath } from "next/cache";
 
-export default function Home() {
-  return <div>My Next App</div>;
+async function addPing() {
+  "use server";
+  await db.insert(pings).values({ message: "Hello from server action" });
+  revalidatePath("/");
+}
+
+export default async function Home() {
+  const recent = await db
+    .select()
+    .from(pings)
+    .orderBy(desc(pings.createdAt))
+    .limit(5);
+
+  return (
+    <div>
+      <form action={addPing}>
+        <Button variant="destructive" type="submit">
+          Submit
+        </Button>
+      </form>
+      <ul>
+        {recent.map((r) => (
+          <li key={r.id}>
+            {" "}
+            {r.id} : {r.message}
+            Created At: {r.createdAt.toISOString()}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 }
